1fb6dc78648ff6e1088ea611c7673a1371588b2f
nmsiddharth <siddharthnm07@gmail.com>
2023-12-01 21:29:41 +0530

    updated crud project

--- class11-crud/src/components/Register.jsx
+++ class11-crud/src/components/Register.jsx
@@ -32,7 +32,7 @@ const Register = () => {
           <div className="col-xs-12 col-sm-12 offset-md-3 col-md-6 offset-lg-3 col-lg-6 offset-xl-3 col-xl-6 offset-xxl-3 col-xxl-6">
             <div className="card">
               <div className="card-body">
-                <form autoComplete='off' onSubmit={submitHandler}>
+                <form autoComplete='on' onSubmit={submitHandler}>
                   <div className="form-group">
                     <label htmlFor="name">Name</label>
                     <input type="text" name='name' id='name' className='form-control' required ref={fName}/>
--- class11-crud/src/data/user.js
+++ class11-crud/src/data/user.js
@@ -1,37 +1,45 @@
 import {toast} from 'react-toastify'
 
 // Initial Data
-const users = JSON.parse(localStorage.getItem("users")) || [] ;
+const users =JSON.parse(localStorage.getItem('users')) || [] ;
 console.log('users',users)
 
 // Register
 const registerUser = async (user) => {
     console.log('Register =', user);
 
-    const extEmail = () => {
-       return users.email === user.email;
-    };
-    const extMobile = () => {
-        return users.mobile === user.mobile;
-     };
+    const extEmail = users.find((item)=> item.email === user.email);
+    const extMobile = users.find((item)=> item.mobile === user.mobile);
 
-    if (extEmail) {
-        toast.warning(`${users.email} already exists`)
+
+    if(extEmail){
+        toast.warning(`${user.email} already exists`);
     }
     else if(extMobile){
-        toast.warning(`${users.mobile} is already registered`);
+        toast.warning(`${user.mobile} already exists`);
     }
     else{
-        user.push(user);
-        saveUsers(user);
-        toast.success(`Hi, ${users.name} you have registered successfully`);
+        users.push(user);
+        saveUsers(users);
+        toast.success(`${user.name} you have successfully registered`);
         window.location.href = "/login";
     }
-    
-};
 
+};
 // Save Data
 const saveUsers = (data) => {
     localStorage.setItem("users", JSON.stringify(data));
+};
+
+const loginUser = async (user) => {
+    const extUser = users.find((item)=> item.email === user.email);
+  
+    if(!extUser){
+        toast.warning(`${user.email} not exists`);
+    }
+    else{
+        toast.success(`${user.email}  exists`);
+
+    }
 }
-export { registerUser } ;
+export { registerUser, loginUser };
