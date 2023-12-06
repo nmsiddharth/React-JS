9e9ca17526281ccce4e4bd5562706e7d27d30bdb
nmsiddharth <siddharthnm07@gmail.com>
2023-12-06 22:39:52 +0530

    updated crud project

--- class11-crud/src/data/task.js
+++ class11-crud/src/data/task.js
@@ -1,7 +1,39 @@
-// Store the Tasks
+import {toast} from "react-toastify";
 
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+// Random ID Generator
+const randId = () => {
+    return Math.floor(Math.random() * 1000);
+}
+
+// Save the Task
+const saveTask = (data) => {
+    localStorage.setItem("tasks", JSON.stringify(data));
+};
+
+// Store the Tasks
 const createTask = async (task) => {
-    console.log("Task.js =", task);
+    // console.log("Task.js =", task);
+    
+    try {
+        const extTask = tasks.find((item)=> item.title === task.title);
+        if (extTask) {
+        toast.warning(`${task.title} already exists`);
+        } 
+        else {
+            const newTask = {
+                id: randId(),
+                ...task,
+            };
+            tasks.push(newTask);
+            saveTask(tasks);
+            toast.success("New task created successfully");
+            window.location.href = "/"; 
+        } 
+    } catch (error) {
+        toast.error(error.message);
+    };
 };
 
 export { createTask };
